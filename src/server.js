be42a0285d02533626749ff04dd5d5283dc2747f
Gajendra Raikwar <gajendra.raikwar.11@gmail.com>
2026-07-05 16:03:59 +0530

    Updated deployment CORS again

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://signflow-frontend-ecru.vercel.app.vercel.app"
+    "https://signflow-frontend-ecru.vercel.app"
   ],
   credentials: true
 }));
