4686bf185219ff810948021f05840248e0cc287b
Gajendra Raikwar <gajendra.raikwar.11@gmail.com>
2026-07-05 15:23:07 +0530

    Updated deployment configuration

--- src/integrations/docusign/docusignClient.js
+++ src/integrations/docusign/docusignClient.js
@@ -1,41 +1,48 @@
 const docusign = require("docusign-esign");
-const fs = require("fs");
-const path = require("path");
 
 const dsApiClient = new docusign.ApiClient();
-dsApiClient.setOAuthBasePath("account-d.docusign.com"); // demo/sandbox auth server
+dsApiClient.setOAuthBasePath("account-d.docusign.com");
 
 const SCOPES = ["signature", "impersonation"];
 
 async function getAuthenticatedClient() {
-const keyPath = path.resolve(__dirname, "../../../", process.env.DOCUSIGN_PRIVATE_KEY_PATH.trim());
-const privateKey = fs.readFileSync(keyPath, "utf8");
+  let privateKey = process.env.DOCUSIGN_PRIVATE_KEY;
 
-  // Step 1: get JWT access token
+  if (!privateKey) {
+    throw new Error("DOCUSIGN_PRIVATE_KEY is missing.");
+  }
+
+  // Safe for both multiline keys and \n formatted keys
+  privateKey = privateKey.replace(/\\n/g, "\n");
+
   const results = await dsApiClient.requestJWTUserToken(
     process.env.DOCUSIGN_INTEGRATION_KEY,
     process.env.DOCUSIGN_USER_ID,
     SCOPES,
     privateKey,
-    3600 // token valid for 1 hour
+    3600
   );
 
   const accessToken = results.body.access_token;
-  dsApiClient.addDefaultHeader("Authorization", "Bearer " + accessToken);
 
-  // Step 2: get real account-specific baseUri (don't hardcode it)
+  dsApiClient.addDefaultHeader(
+    "Authorization",
+    `Bearer ${accessToken}`
+  );
+
   const userInfo = await dsApiClient.getUserInfo(accessToken);
-  const account = userInfo.accounts.find(
-    (acc) => acc.accountId === process.env.DOCUSIGN_ACCOUNT_ID
-  ) || userInfo.accounts[0];
 
-  const baseUri = `${account.baseUri}/restapi`;
-  dsApiClient.setBasePath(baseUri);
+  const account =
+    userInfo.accounts.find(
+      (acc) => acc.accountId === process.env.DOCUSIGN_ACCOUNT_ID
+    ) || userInfo.accounts[0];
+
+  dsApiClient.setBasePath(`${account.baseUri}/restapi`);
 
   return {
     apiClient: dsApiClient,
     accountId: account.accountId,
-    baseUri,
+    baseUri: `${account.baseUri}/restapi`,
     accessToken,
   };
 }
